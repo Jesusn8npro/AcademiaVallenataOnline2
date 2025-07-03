37aa2fddbdec034d28bcee49784dafd97b62b4fd
jesusn8npro <acordeon91@gmail.com>
2025-07-03 12:05:46 -0500

    fix: Simplify to basic SvelteKit config

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
 	kit: { 
-		adapter: adapter({
-			// Configuración específica para producción en contenedores
-			out: 'build',
-			precompress: false,
-			envPrefix: ''
-		}),
+		adapter: adapter(),
 		alias: {
 			'$lib': 'src/lib'
 		},
